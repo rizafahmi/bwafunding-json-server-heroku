c9481b633b283f938245571b4c6e4b3c6b7fe644
Riza Fahmi <rizafahmi@gmail.com>
2020-08-02 14:38:33 +0700

    Add cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
+server.use(cors());
 server.use(router);
 
 server.listen(port);
