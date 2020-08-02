cfedf7c0b430c90b350c7c1d9d4b7028448ba463
Riza Fahmi <rizafahmi@gmail.com>
2020-08-02 14:56:08 +0700

    cORS

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({noCors: false});
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
